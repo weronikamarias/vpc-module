1004639a57d0b57ab7e56d3cb03508a440dae578
weronikamarias <72075691+weronikamarias@users.noreply.github.com>
2025-08-27 13:57:25 +0200

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,36 @@
+# outputs.tf - Output values from VPC Module
+
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+
+output "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  value       = aws_vpc.main.cidr_block
+}
+
+output "public_subnet_ids" {
+  description = "IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = aws_internet_gateway.main.id
+}
+
+output "public_route_table_id" {
+  description = "ID of the public route table"
+  value       = aws_route_table.public.id
+}
+
+output "availability_zones" {
+  description = "Availability zones used"
+  value       = var.availability_zones
+}
